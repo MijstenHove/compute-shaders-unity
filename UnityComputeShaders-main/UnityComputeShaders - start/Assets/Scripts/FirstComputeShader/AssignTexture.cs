449d3a6ad195a9161b5ab2b187c0ad99effc47cd
MijstenHove <mijs.tenhove@hotmail.com>
2023-03-03 19:57:56 +0100

    First compute Shader

---
+++ UnityComputeShaders-main/UnityComputeShaders - start/Assets/Scripts/FirstComputeShader/AssignTexture.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignTexture : MonoBehaviour
+{
+
+    public ComputeShader shader;
+    public int textResolution = 256;
+
+    Renderer rend;
+    RenderTexture outputTexture;
+    int kernelHandel;
+
+    void Start()
+    {
+        outputTexture = new RenderTexture(textResolution, textResolution, 0);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+        InitShader();
+
+	}
+
+    private void InitShader()
+    {
+        kernelHandel = shader.FindKernel("CSMain");
+        shader.SetTexture(kernelHandel, "Result", outputTexture);
+        rend.material.SetTexture("_MainTex", outputTexture);
+
+        DispatchShader(textResolution / 16, textResolution / 16);
+    
+    }
+
+    private void DispatchShader(int x, int y)
+    {
+
+        shader.Dispatch(kernelHandel, x, y, 1);
+
+    }
+
+
+	void Update()
+    {
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            DispatchShader(textResolution / 8, textResolution / 8);
+        
+        }
+
+    }
+}
